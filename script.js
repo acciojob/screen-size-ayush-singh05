952594e126c35752f5d4a49c664e25ca156a5f13
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-02 12:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 const h1 = document.getElementById("sizeInfo");
 const width = window.innerWidth;
 const height = window.innerHeight;
-h1.innerText = `width:${width} and Height: ${height} `;
+h1.innerText = `Width: ${width} and Height: ${height} `;
+
