9e86fa01a1e1a5ca89989145cbe3af3a7d043e15
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-02 13:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your JS code here. If required.
-const h1 = document.getElementById("sizeInfo");
+function updateWindow(){
+	const h1 = document.getElementById("sizeInfo");
 const width = window.innerWidth;
 const height = window.innerHeight;
 h1.innerText = `Width: ${width} and Height: ${height} `;
+}
 
+window.addEventListener('resize', updateWindow);
+
+// Initial call to set the initial window dimensions
+updateWindow();
