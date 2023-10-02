7118538351495b4a3a5960c588406ed590d08133
Ayush Singh <ayushsingh2442@gmail.com>
2023-10-02 13:07:49 +0530

    submit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 function updateWindow(){
 	const h1 = document.getElementById("sizeInfo");
-const width = window.innerWidth;
-const height = window.innerHeight;
-h1.innerText = `Width: ${width} and Height: ${height} `;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    h1.innerText = `Width: ${width} and Height: ${height} `;
 }
 
 window.addEventListener('resize', updateWindow);
