a1d5b6d456352d9c0dd52a751d76688353cf80f1
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-02 12:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const h1 = document.getElementById("sizeInfo");
+const width = window.innerWidth;
+const height = window.innerHeight;
+h1.innerText = `width:${width} and Height: ${height} `;
